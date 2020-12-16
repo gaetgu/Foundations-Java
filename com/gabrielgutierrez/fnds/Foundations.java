7e33edcb5775157673f9b633c1ce696d3e91a302
gaetgu <gabriel.ethan@gmail.com>
2020-12-15 18:53:42 -0700

    :D

--- com/gabrielgutierrez/fnds/Foundations.java
+++ com/gabrielgutierrez/fnds/Foundations.java
@@ -1,4 +1,5 @@
 package com.gabrielgutierrez.fnds;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export PATH="$HOME/Foundations-Java/com/gabrielgutierrez/fnds:$PATH"
-echo 'alias fnds="Foundations.java"' > ~/.zshrc
